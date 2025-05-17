ff86b4a8d4396f14d63ac3807489ba4ac5eb9741
RobbeDGreef <robbedegreef@gmail.com>
2025-05-17 12:15:44 +0200

    fix: attributes on items were not printed / written to tokenstream
    Signed-off-by: RobbeDGreef <robbedegreef@gmail.com>

--- src/stmt.rs
+++ src/stmt.rs
@@ -2534,6 +2534,9 @@ impl<K> AddVisibility<K> for Item<K> {
 
 impl<K: fmt::Display> fmt::Display for Item<K> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for attr in &self.attrs {
+            writeln!(f, "{attr}")?;
+        }
         write!(f, "{}{}", self.vis, self.kind)
     }
 }
@@ -2541,6 +2544,9 @@ impl<K: fmt::Display> fmt::Display for Item<K> {
 impl<K: Into<TokenStream>> From<Item<K>> for TokenStream {
     fn from(value: Item<K>) -> Self {
         let mut ts = TokenStream::new();
+        for attr in value.attrs {
+            ts.extend(TokenStream::from(attr))
+        }
         ts.extend(TokenStream::from(value.vis));
         ts.extend(value.kind.into());
         ts
